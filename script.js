f65eb972bd0b2f1ad1c0084ad6cdeeee895518c5
mmartinez1468 <165759311+mmartinez1468@users.noreply.github.com>
2025-02-26 20:31:10 +0100

    Update

--- script.js
+++ script.js
@@ -26,6 +26,23 @@ const observer = new IntersectionObserver(entries => {
   });
 
 
+  /*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
+
+ 
 ///////////////// NUMBER INCREASE ANIMATION /////////////////
 let valueDisplays = document.querySelectorAll('.statNum');
 let interval = 4000;
