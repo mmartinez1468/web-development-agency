eb48fc5af75b2d352fecc6d62135a84ba130c2e1
mmartinez1468 <165759311+mmartinez1468@users.noreply.github.com>
2025-02-26 02:45:46 +0100

    ORGANIZE

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+///////////////// PRE LOADER /////////////////
+window.addEventListener('load', () => {
+    const loader = document.querySelector('.loader');
+    
+    loader.classList.add('loaderHidden');
+  
+    loader.addEventListener('transitioned', () => {
+      document.body.removeChild('loader');
+    })
+  })
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        this.parentElement.classList.toggle("active");
 
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+///////////////// REPLAYS ANIMATION WHEN BACK IN VIEWPORT /////////////////
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible'); // Remove to re-trigger animation
+      }
     });
-}
+  }, { threshold: 0.2 });
+  
+  document.querySelectorAll('.animated').forEach(element => {
+    observer.observe(element);
+  });
 
 
+///////////////// NUMBER INCREASE ANIMATION /////////////////
+let valueDisplays = document.querySelectorAll('.statNum');
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute('data-val'));
+    let increment = 1;  // Slower increment
+    let duration = Math.floor(interval / (endValue / increment));
+
+    let counter = setInterval(function () {
+        startValue += increment;
+        valueDisplay.textContent = Math.round(startValue) + "%"; 
+        if (startValue >= endValue) {
+            clearInterval(counter);
+            valueDisplay.textContent = endValue + "%";  // Ensure it ends exactly at 100%
+        }
+    }, duration);
+});
+console.log(document.querySelectorAll('.statNum'));
 
 
-
+///////////////// IMAGE SLIDER IN PRICING SECTION /////////////////
 document.addEventListener('DOMContentLoaded', function() {
   const sliderContainer = document.querySelector('.pricing-slider-container');
   const sliderDots = document.querySelectorAll('.slider-dot');
@@ -81,64 +111,23 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+///////////////// OPENS UP FAQ CONTENT /////////////////
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        this.parentElement.classList.toggle("active");
 
-
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      } else {
-        entry.target.classList.remove('visible'); // Remove to re-trigger animation
-      }
-    });
-  }, { threshold: 0.2 });
-  
-  document.querySelectorAll('.animated').forEach(element => {
-    observer.observe(element);
-  });
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let valueDisplays = document.querySelectorAll('.statNum');
-let interval = 4000;
-
-valueDisplays.forEach((valueDisplay) => {
-    let startValue = 0;
-    let endValue = parseInt(valueDisplay.getAttribute('data-val'));
-    let increment = 1;  // Slower increment
-    let duration = Math.floor(interval / (endValue / increment));
-
-    let counter = setInterval(function () {
-        startValue += increment;
-        valueDisplay.textContent = Math.round(startValue) + "%"; 
-        if (startValue >= endValue) {
-            clearInterval(counter);
-            valueDisplay.textContent = endValue + "%";  // Ensure it ends exactly at 100%
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
         }
-    }, duration);
-});
-console.log(document.querySelectorAll('.statNum'));
+    });
+}
 
 
 
@@ -146,12 +135,26 @@ console.log(document.querySelectorAll('.statNum'));
 
 
 
-window.addEventListener('load', () => {
-  const loader = document.querySelector('.loader');
   
-  loader.classList.add('loaderHidden');
 
-  loader.addEventListener('transitioned', () => {
-    document.body.removeChild('loader');
-  })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
